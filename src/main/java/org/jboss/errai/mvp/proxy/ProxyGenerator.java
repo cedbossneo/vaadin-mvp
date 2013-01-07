1dcf770f6f53c6eca8210fe7d78e64bbb22469ec
cedric <hauber.c@gmail.com>
2013-01-07 11:59:07 +0100

    Fix event generation

--- src/main/java/org/jboss/errai/mvp/proxy/ProxyGenerator.java
+++ src/main/java/org/jboss/errai/mvp/proxy/ProxyGenerator.java
@@ -33,12 +33,12 @@ public class ProxyGenerator implements Serializable{
         try {
             CtClass proxyImplClass = poll.getCtClass(ProxyImpl.class.getName());
             CtClass proxyClass = poll.makeClass(presenter.getSimpleName() + "Proxy", proxyImplClass);
-            CtConstructor constructor = CtNewConstructor.make("" +
-                    "public " + presenter.getSimpleName() + "Proxy(com.google.web.bindery.event.shared.EventBus eventBus) {\n" +
-                    "      super(" + presenter.getName() + ".class, eventBus);\n" +
-                    "  }", proxyClass);
-            registerEventsHandler(poll, presenter, proxyClass, constructor);
-            proxyClass.addConstructor(constructor);
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append("public " + presenter.getSimpleName() + "Proxy(com.google.web.bindery.event.shared.EventBus eventBus) {");
+            stringBuffer.append("   super(" + presenter.getName() + ".class, eventBus);");
+            stringBuffer.append(registerEventsHandler(poll, presenter, proxyClass));
+            stringBuffer.append("}");
+            proxyClass.addConstructor(CtNewConstructor.make(stringBuffer.toString(), proxyClass));
             Class finalProxyClass = proxyClass.toClass();
             proxies.put(presenter, finalProxyClass);
             return finalProxyClass;
@@ -50,12 +50,14 @@ public class ProxyGenerator implements Serializable{
         return null;
     }
 
-    private <P extends Presenter<? extends View>> void registerEventsHandler(ClassPool pool, Class<P> presenter, CtClass proxyClass, CtConstructor constructor) {
+    private <P extends Presenter<? extends View>> StringBuffer registerEventsHandler(ClassPool pool, Class<P> presenter, CtClass proxyClass) {
+        StringBuffer stringBuffer = new StringBuffer();
         Method[] methods = presenter.getMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(ProxyEvent.class))
                 try {
-                    if (method.getParameterTypes()[0].getMethod("getType").getModifiers() == Modifier.STATIC)
+                    int modifiers = method.getParameterTypes()[0].getMethod("getType").getModifiers();
+                    if (Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers))
                         try {
                             proxyClass.addInterface(pool.getCtClass(getHandler(presenter, method).getName()));
                             String event = method.getParameterTypes()[0].getName();
@@ -63,7 +65,7 @@ public class ProxyGenerator implements Serializable{
                                     "public void "+method.getName()+"("+ event +" event){"+
                                             "       invokeMethod(\""+method.getName()+"\", event, "+event+".class);" +
                                             "}", proxyClass));
-                            constructor.insertAfter("getEventBus().addHandler("+event+".getType(), this);");
+                            stringBuffer.append("getEventBus().addHandler(" + event + ".getType(), this);");
                         } catch (NotFoundException e) {
                             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                         } catch (CannotCompileException e) {
@@ -73,6 +75,7 @@ public class ProxyGenerator implements Serializable{
                     System.out.println("Not such static method getType for Event " + method.getName());
                 }
         }
+        return stringBuffer;
     }
 
     private <P extends Presenter<? extends View>> Class<?> getHandler(Class<P> presenter, Method method) {
