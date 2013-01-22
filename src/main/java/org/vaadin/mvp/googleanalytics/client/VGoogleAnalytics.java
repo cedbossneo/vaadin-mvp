66fbe8ff7b74e07450bc31326be16e0462fdb765
cedric <hauber.c@gmail.com>
2013-01-22 16:59:38 +0100

    Rename packages

---
+++ src/main/java/org/vaadin/mvp/googleanalytics/client/VGoogleAnalytics.java
@@ -0,0 +1,76 @@
+package org.vaadin.mvp.googleanalytics.client;
+
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.vaadin.client.BrowserInfo;
+import com.vaadin.client.ui.AbstractComponentConnector;
+import com.vaadin.shared.ui.Connect;
+import org.vaadin.mvp.googleanalytics.GoogleAnalyticsTracker;
+
+@Connect(value = GoogleAnalyticsTracker.class)
+public class VGoogleAnalytics extends AbstractComponentConnector{
+
+    @Override
+    public GoogleAnalyticsState getState() {
+        return (GoogleAnalyticsState) super.getState();
+    }
+
+    @Override
+    public SimplePanel getWidget() {
+        return (SimplePanel) super.getWidget();
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        RootPanel.get().add(getWidget());
+        if (BrowserInfo.get().isIE() && BrowserInfo.get().getIEVersion() == 6) {
+            getWidget().getElement().getStyle().setProperty("overflow", "hidden");
+            getWidget().getElement().getStyle().setProperty("height", "0");
+            getWidget().getElement().getStyle().setProperty("width", "0");
+        }
+        registerRpc(GoogleAnalyticsRPC.class, new GoogleAnalyticsRPC() {
+            @Override
+            public void trackPageview(String pageId) {
+                _trackPageview(getState().getTrackerId(), pageId, getState().getDomainName(), getState().isAllowAnchor());
+            }
+        });
+    }
+
+    /** Native JS call to invoke _trackPageview from ga.js.
+     *
+     * @param trackerId
+     * @param pageId
+     * @param domainName
+     * @param allowAnchor
+     * @return
+     */
+    private native String _trackPageview(String trackerId, String pageId,
+                                        String domainName, boolean allowAnchor)
+    /*-{
+        if (!$wnd._gat) {
+            return "Tracker not found (running offline?)";
+        }
+        try {
+            var pageTracker = $wnd._gat._getTracker(trackerId);
+            if (!pageTracker) {
+                return "Failed to get tracker for "+trackerId;
+            }
+
+            if (domainName) {
+                pageTracker._setDomainName(domainName);
+            }
+
+            pageTracker._setAllowAnchor(allowAnchor);
+
+            if (pageId) {
+                pageTracker._trackPageview(pageId);
+            } else {
+                pageTracker._trackPageview();
+            }
+            return null;
+        } catch(err) {
+            return ""+err;
+        }
+    }-*/;
+}
---
+++ src/main/java/org/vaadin/mvp/views/PopupViewImpl.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2012 Cedric Hauber
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.vaadin.mvp.views;
+
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Window;
+import org.vaadin.mvp.events.NavigationEvent;
+import org.vaadin.mvp.events.NavigationHandler;
+
+/**
+ * A simple implementation of {@link PopupView} that can be used when the widget
+ * returned by {@link #asComponent()} ()} inherits from {@link com.google.gwt.user.client.ui.PopupPanel}.
+ *
+ * Also, this implementation simply disregards every call to
+ * {@link #setInSlot(Object, com.google.gwt.user.client.ui.Widget)}, {@link #addToSlot(Object, com.google.gwt.user.client.ui.Widget)}, and
+ * {@link #removeFromSlot(Object, com.google.gwt.user.client.ui.Widget)}.
+ *
+ * @author Philippe Beaudoin
+ */
+public abstract class PopupViewImpl extends ViewImpl implements PopupView {
+
+  private HandlerRegistration autoHideHandler;
+
+  private Window.CloseListener closeListener;
+  private final EventBus eventBus;
+
+  /**
+   * The {@link PopupViewImpl} class uses the {@link com.google.web.bindery.event.shared.EventBus} to listen to
+   * {@link NavigationEvent} in order to automatically close when this event is
+   * fired, if desired. See
+   * {@link #setAutoHideOnNavigationEventEnabled(boolean)} for details.
+   *
+   * @param eventBus The {@link com.google.web.bindery.event.shared.EventBus}.
+   */
+  protected PopupViewImpl(EventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  @Override
+  public void center() {
+    doCenter();
+  }
+
+  @Override
+  public void hide() {
+    asWindow().close();
+  }
+
+  @Override
+  public void setAutoHideOnNavigationEventEnabled(boolean autoHide) {
+    if (autoHide) {
+      if (autoHideHandler != null) {
+        return;
+      }
+      autoHideHandler = eventBus.addHandler(NavigationEvent.getType(),
+          new NavigationHandler() {
+            @Override
+            public void onNavigation(NavigationEvent navigationEvent) {
+              hide();
+            }
+          });
+    } else {
+      if (autoHideHandler != null) {
+        autoHideHandler.removeHandler();
+      }
+    }
+  }
+
+  @Override
+  public void setCloseHandler(final PopupViewCloseHandler popupViewCloseHandler) {
+    if (closeListener != null) {
+      asWindow().removeCloseListener(closeListener);
+    }
+    if (popupViewCloseHandler == null) {
+      closeListener = null;
+    } else {
+        closeListener = new Window.CloseListener() {
+            @Override
+            public void windowClose(Window.CloseEvent e) {
+                popupViewCloseHandler.onClose();
+            }
+        };
+        asWindow().addCloseListener(closeListener);
+    }
+  }
+
+  @Override
+  public void setPosition(int left, int top) {
+    asWindow().setPositionX(left);
+    asWindow().setPositionY(top);
+  }
+
+  @Override
+  public void show() {
+    UI.getCurrent().addWindow(asWindow());
+  }
+
+  /**
+   * Retrieves this view as a {@link com.google.gwt.user.client.ui.PopupPanel}. See {@link #asWidget()}.
+   *
+   * @return This view as a {@link com.google.gwt.user.client.ui.PopupPanel} object.
+   */
+  protected Window asWindow() {
+    return (Window) asComponent();
+  }
+
+  /**
+   * This method centers the popup panel, temporarily making it visible if
+   * needed.
+   */
+  private void doCenter() {
+    boolean wasVisible = asWindow().isVisible();
+    asWindow().center();
+    if (!wasVisible) {
+      asWindow().close();
+    }
+  }
+}
